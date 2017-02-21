b95728c2c43796875ba1dccd71a631faa3df59a8
Clément D <neo.area.prog@gmail.com>
2017-02-21 23:25:11 +0100

    Premier commit (bases projet)

---
+++ android/src/com/convicted/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.convicted.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.convicted.game.ConvictedGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ConvictedGame(), config);
+	}
+}
