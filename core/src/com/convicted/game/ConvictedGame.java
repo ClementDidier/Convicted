50de62ac72eecd8bb9490c494797399596604ea3
Clément D <neo.area.prog@gmail.com>
2017-07-03 17:56:39 +0200

    Persistence configurations & Centralisation
    * Configuration permettant d'initialiser les valeurs si nécessaires et
    de les obtenir ultérieurement
    * Fusion de Convicted et ConvictedGame

--- core/src/com/convicted/game/ConvictedGame.java
+++ core/src/com/convicted/game/ConvictedGame.java
@@ -1,16 +1,23 @@
 package com.convicted.game;
 
 import com.badlogic.gdx.Game;
+import com.convicted.game.data.Configuration;
 import com.convicted.game.drawable.ui.screen.ConvictedScreen;
 import com.convicted.game.drawable.ui.screen.ScreenNavigator;
+import com.convicted.game.data.ConvictedAssetManager;
 
 public class ConvictedGame extends Game
 {
+	private static ConvictedGame game;
+
 	private ConvictedScreen screen;
+	private Configuration config;
+	private ConvictedAssetManager assetManager;
 
 	@Override
 	public void create ()
 	{
+		this.assetManager = new ConvictedAssetManager();
 		ScreenNavigator.navigateTo(ConvictedScreen.SPLASH);
 	}
 
@@ -28,6 +35,12 @@ public class ConvictedGame extends Game
 			this.screen.dispose();
 	}
 
+	@Override
+	public ConvictedScreen getScreen()
+	{
+		return this.screen;
+	}
+
 	public void setScreen(ConvictedScreen screen)
 	{
 		screen.initialize();
@@ -36,15 +49,40 @@ public class ConvictedGame extends Game
 	}
 
 	@Override
-	public ConvictedScreen getScreen()
-	{
-		return this.screen;
-	}
-
-	@Override
 	@Deprecated
 	public final void setScreen(com.badlogic.gdx.Screen screen)
 	{
 		throw new UnsupportedOperationException("Méthode \"setScreen(Screen)\" non supportée avec gdx.Screen en paramètre");
+	}
+
+	/**
+	 * Obtient une instance de configuration de jeu
+	 * @return L'instance de configuration
+	 */
+	public Configuration getConfiguration()
+	{
+		if(config == null)
+			config = new Configuration();
+		return config;
+	}
+
+	/**
+	 * Obtient le gestionnaire de dépendances
+	 * @return Le gestionnaire de dépendances
+	 */
+	public ConvictedAssetManager getAssetManager()
+	{
+		return this.assetManager;
+	}
+
+	/**
+	 * Initialise une instance du jeu si necessaire et la retourne
+	 * @return L'instance du jeu
+	 */
+	public static ConvictedGame getInstance()
+	{
+		if(game == null)
+			game = new ConvictedGame();
+		return game;
 	}
 }
--- core/src/com/convicted/game/data/Configuration.java
+++ core/src/com/convicted/game/data/Configuration.java
@@ -1,6 +1,73 @@
 package com.convicted.game.data;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.convicted.game.drawable.ui.screen.ConvictedScreen;
+
 public class Configuration
 {
+    private final static int DEFAULT_INT_VALUE = 0;
+    private final static float DEFAULT_FLOAT_VALUE = 0f;
+    private final static long DEFAULT_LONG_VALUE = 0;
+    private final static boolean DEFAULT_BOOLEAN_VALUE = false;
 
+    public final static String PREFS_NAME = "ConvictedPrefs";
+    public final static String PREFS_MOVE_JOYSTICK_ALIGN_X = "MoveJoystickAlignX";
+    public final static String PREFS_MOVE_JOYSTICK_ALIGN_Y = "MoveJoystickAlignY";
+    public final static String PREFS_SPLASH_PROGRESS_BAR_ALIGN_X = "SplashProgressBarAlignX";
+    public final static String PREFS_SPLASH_PROGRESS_BAR_ALIGN_Y = "SplashProgressBarAlignY";
+
+    private static Configuration CONFIG;
+
+    private Preferences prefs;
+
+    public Configuration()
+    {
+        this.prefs = Gdx.app.getPreferences(PREFS_NAME);
+        this.initialize();
+    }
+
+    public static Configuration getInstance()
+    {
+        if(CONFIG == null)
+            CONFIG = new Configuration();
+        return CONFIG;
+    }
+
+    public int getInteger(String content)
+    {
+        return this.prefs.getInteger(content, DEFAULT_INT_VALUE);
+    }
+
+    public boolean getBoolean(String content)
+    {
+        return this.prefs.getBoolean(content, DEFAULT_BOOLEAN_VALUE);
+    }
+
+    public float getFloat(String content)
+    {
+        return this.prefs.getFloat(content, DEFAULT_FLOAT_VALUE);
+    }
+
+    public long getLong(String content)
+    {
+        return this.prefs.getLong(content, DEFAULT_LONG_VALUE);
+    }
+
+    public void initialize()
+    {
+        if(!this.prefs.contains(PREFS_MOVE_JOYSTICK_ALIGN_X))
+            this.prefs.putFloat(PREFS_MOVE_JOYSTICK_ALIGN_X, ConvictedScreen.VIEWPORT.x / 7);
+
+        if(!this.prefs.contains(PREFS_MOVE_JOYSTICK_ALIGN_Y))
+            this.prefs.putFloat(PREFS_MOVE_JOYSTICK_ALIGN_Y, ConvictedScreen.VIEWPORT.y / 5);
+
+        if(!this.prefs.contains(PREFS_SPLASH_PROGRESS_BAR_ALIGN_X))
+            this.prefs.putInteger(PREFS_SPLASH_PROGRESS_BAR_ALIGN_X, (int)(ConvictedScreen.VIEWPORT.x / 2 - 655 / 2));
+
+        if(!this.prefs.contains(PREFS_SPLASH_PROGRESS_BAR_ALIGN_Y))
+            this.prefs.putInteger(PREFS_SPLASH_PROGRESS_BAR_ALIGN_Y, 260);
+
+        this.prefs.flush();
+    }
 }
